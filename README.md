ed14115d9ee16534018f64da179ea9972aab3f28
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2021-09-10 14:56:47 +0430

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Node.js TypeScript Starter Kit
+
+## Usage
+1. Install dependencies with `yarn`
+2. Create TXT dependencies with `touch domains.txt` and `touch free_domains.txt`
+3. Build JavaScript with `yarn build-ts`
+4. Run with `node dist/index.js`
+
+
+## Develop
+You can change letter and count domain with change code in `index.ts line 28` and rebuild with `yarn build-ts`
+
+## Error
+If you have an error, You can see on `.log` file
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { red as R, green as G, blue as B } from "colors";
 import axios from "axios";
-import { appendFile } from "fs";
+import { appendFile, readFileSync } from "fs";
 
-export const genWordlist = function (n: number, letters: string) {
+const genWordlist = function (n: number, letters: string) {
   let results: Array<string> = [];
 
   const helper = function (cache: string) {
@@ -20,22 +20,46 @@ export const genWordlist = function (n: number, letters: string) {
   return results;
 };
 
+let exists_list: Array<string> = [];
+readFileSync('domains.txt', 'utf-8').split(/\r?\n/).forEach(function (line) {
+  exists_list.push(line.replace('\n', ''))
+})
+
 const list = genWordlist(3, "abcdefghijklmnopqrstuvwxyz123456789");
 let count = 0;
-const check = (domain_name: string, list_count: number) => axios.get(`http://api.whoapi.com/?apikey=5e5345ee4cbc7db2f752e7598a933d02&r=whois&domain=${domain_name}.ir`)
-  .then(function (response) {
-    if (response.data.registered === false) {
-      appendFile('./free_domain.txt',
-        `${response.data.domain_name}\n`
-        , () => { });
-    }
+const check = (domain_name: string, list_count: number) => {
+  if (exists_list.includes(`${domain_name}.ir`)) {
     console.log(
-      `${B('[')}${G(String(count))}${B(']')} ${response.data.domain_name}${(response.data.registered === false) ? G(' Free') : R(' Registered')}${(response.data.premium === true) ? R(' PREMIUM') : ""}`
+      `${B('[')}${G(String(count))}${B(']')} ${domain_name}${G(' Checked')}`
     );
     count++;
-    check(list[list_count + 1], list_count + 1);
-  }).catch(function (error) {
-    console.error(error);
-  });
+    if (list_count < list.length) {
+      check(list[list_count + 1], list_count + 1);
+    }
+  } else {
+    axios.get(`http://api.whoapi.com/?apikey=5e5345ee4cbc7db2f752e7598a933d02&r=whois&domain=${domain_name}.ir`)
+      .then(function (response) {
+        if (response.data.registered === false) {
+          appendFile('./free_domains.txt',
+            `${response.data.domain_name}\n`
+            , () => { });
+        }
+        appendFile('./domains.txt',
+          `${response.data.domain_name}\n`
+          , () => { });
+        console.log(
+          `${B('[')}${G(String(count))}${B(']')} ${response.data.domain_name}${(response.data.registered === false) ? G(' Free') : R(' Registered')}${(response.data.premium === true) ? R(' PREMIUM') : ""}`
+        );
+        count++;
+        if (list_count < list.length) {
+          check(list[list_count + 1], list_count + 1);
+        }
+      }).catch(function (error) {
+        appendFile('.log', error, () => { });
+        console.error(error.response.status);
+      });
+
+  }
+}
 
 check(list[0], 0)
